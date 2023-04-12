ec5f14505edfdd96c1bc0aceb50c427bf00e80e5
Aayush Rijal <arijal@aiims.com.au>
2023-04-12 11:05:57 +1000

    Header updated

--- app/src/header.php
+++ app/src/header.php
@@ -8,13 +8,6 @@
 	<link rel="icon" href="./assets/images/favicon.png" type="image/png">
 	<title><?= $site ?></title>
 
-	<?php if ($application_page) : ?>
-		<link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;600&display=swap" rel="stylesheet">
-		<link rel="stylesheet" href="https://s3.ap-southeast-2.amazonaws.com/widget.driveiq/assets/css/all.css">
-		<link rel="stylesheet" href="https://s3.ap-southeast-2.amazonaws.com/widget.driveiq/widgetApplication/css/2.f858cb61.chunk.css">
-		<link rel="stylesheet" href="https://s3.ap-southeast-2.amazonaws.com/widget.driveiq/widgetApplication/css/main.aa521ec1.chunk.css">
-	<?php endif; ?>
-
 	<link rel="stylesheet" href="./assets/css/main.css?v=0.1">
 
 	<!-- Recaptcha Here -->
